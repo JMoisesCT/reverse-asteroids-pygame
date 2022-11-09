7c100eb6a4135c59e3fc6efaafd4b4bff31392bb
Moises Cumpa Torres <moises.cumpa@gmail.com>
2022-11-09 00:27:05 -0500

    Updating main loop of game

--- scripts/game.py
+++ scripts/game.py
@@ -1,3 +1,31 @@
-a = 3
-b = a + 4
-print(f'{b=}')
+import pygame as pg
+
+HEIGHT = 256
+WIDTH = 256
+FPS = 60
+
+def main():
+    pg.init()
+    clock = pg.time.Clock()
+    displaysurface = pg.display.set_mode((WIDTH, HEIGHT), pg.SCALED)
+    pg.display.set_caption("Reverse Asteroids")
+
+     # Main Loop
+    updating = True
+    while updating:
+        clock.tick(FPS)
+
+        # Handle Input Events
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                updating = False
+
+        displaysurface.fill((0, 0, 0))
+
+        pg.display.flip()
+
+    pg.quit()
+
+# this calls the 'main' function when this script is executed
+if __name__ == "__main__":
+    main()
